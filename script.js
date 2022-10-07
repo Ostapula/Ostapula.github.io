0b01c975303930b142ab52d9b4bb55a6c766605d
Ostapula <ostapnote2@gmail.com>
2022-10-07 23:03:22 +0300

    test

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+import data from './list.json' assert { type: 'JSON' };
+console.log(data);
+
 let addButton = document.getElementById('submitBtn');
 let shopList = document.getElementById('shopList');
 let inputField = document.getElementById('inputField');
+let clicked = false;
 
 addButton.addEventListener('click', function(){
     var listAdd = document.createElement('li');
@@ -9,7 +13,13 @@ addButton.addEventListener('click', function(){
     shopList.appendChild(listAdd);
     inputField.value = "";
     listAdd.addEventListener('click', function(){
-        listAdd.style.textDecoration = "line-through";
+        if(clicked == false){
+            listAdd.style.textDecoration = "line-through";
+            clicked = true;
+        }   else {
+            listAdd.style.textDecoration = "none";
+            clicked = false;
+        }
     })
     listAdd.addEventListener('dblclick', function(){
         shopList.removeChild(listAdd);
