8ef5fa375d18a8002060ea95a3b70a4756d6644d
Ostap <82474195+Ostapula@users.noreply.github.com>
2023-04-20 00:19:32 +0300

    Add html, css, js and image

---
+++ script.js
@@ -0,0 +1,51 @@
+
+let poopBoolToday = false;
+let date = new Date();
+function dateChecker(){
+    return date.getDate();
+}
+
+function hideButtons(){
+    document.getElementById("buttonYes").style.visibility = 'hidden';
+    document.getElementById("buttonNo").style.visibility = 'hidden';
+}
+
+function yesPoop() {
+    document.getElementById("main-text").innerHTML = 'Молодець!';
+    document.getElementById("body").style.backgroundColor = '#964B00';
+    poopBoolToday = true;
+    localStorage.setItem('poop', poopBoolToday.toString());
+    hideButtons();
+    localStorage.setItem('date', date.getDate().toString());
+}
+
+function noPoop() {
+    document.getElementById("main-text").innerHTML = 'Шкода...';
+    document.getElementById("body").style.backgroundColor = 'white';
+    poopBoolToday = false;
+    localStorage.setItem('poop', poopBoolToday.toString());
+}
+
+function resetPoop(){
+    poopBoolToday = null;
+    localStorage.clear();
+    console.log('Local storage reseted');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const raw = localStorage.getItem('poop');
+    const outputRaw = JSON.parse(raw);
+    const rawDate = localStorage.getItem('date');
+    const outputRawDate = JSON.parse(rawDate);
+    if (outputRawDate !== dateChecker()){
+        resetPoop();
+    }
+    else if (outputRaw === true){
+        yesPoop();
+    } else if (outputRaw === false) {
+        noPoop();
+    }
+    console.log(outputRaw);
+    console.log(outputRawDate);
+    console.log(dateChecker());
+});
